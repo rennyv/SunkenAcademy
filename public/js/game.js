6f09584f2679c11370b708cdc2566467c2f202bd
Renny <renny@vanneste.ca>
2016-06-20 14:24:36 -0600

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -117,6 +117,7 @@ window.onload = function () {
         "--" + player.name + " the " + player.characterClass +
         "<br />--HP: " + player.hp + "/" + player.levelStats[player.level].maxHp +
         "<br />--MP: " + player.mp + "/" + player.levelStats[player.level].maxMp +  
+        "<br />--Atk: " + player.attack +
         "<br />--Exp: " + player.exp +
         "<br />--Level: " + player.level +
         "<br />--GP: " + player.gp +
